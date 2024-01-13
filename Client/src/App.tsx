1ddd2f63445757f5b2dccc7215cebdcbeb614791
om thorat <omthorat1005@gmail.com>
2024-01-12 17:14:58 -0800

    first commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+
+const router = createBrowserRouter([
+  
+])
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
